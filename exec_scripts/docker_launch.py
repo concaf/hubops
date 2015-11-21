c5fbc08aab65bd1bae4fcfbaa768ecf53cecacaf
Contains Cafeine <shubham@linux.com>
2015-11-21 16:23:34 +0530

    Simplified code, implemented functions, included supplied tag for remote pull.

--- exec_scripts/docker_launch.py
+++ exec_scripts/docker_launch.py
@@ -1,17 +1,74 @@
 # /usr/bin/python3
 
 from docker import Client
-import json
-import requests, sys
+import json, requests, sys
 
 sys.path.append("/root/hubops")
 from config.docker_config import docker_config
 from sys import argv
+from random import randint
 
 
-def search_images_local(image_list, user_repotag):
+class ContainerClass:
+    def __init__(self):
+        self.container_name = "hubops{}".format(randint(100000,999999))
+        self.container_hostname = self.container_name
+        self.container_dns = ["8.8.8.8", "8.8.4.4"]
+        self.container_mem_limit = "512m"
+        self.container_command = "/bin/bash"
+
+
+def get_image_remote(client_instance, image_repository, image_repository_tags):
+    # User wants official or unofficial images?
+    while True:
+        official_or_not = input("Search for official images or unofficial images?\n"
+                                "Official(O)\n"
+                                "Unofficial(U)\n").lower()
+        if official_or_not in list("ou"):
+            break
+
+    found_list = search_images_name_remote(image_repository, client_instance, official_or_not)
+
+    if len(found_list) == 1:
+        print("No official image found for {}".format(image_repository))
+    else:
+        print("Official images on Docker Hub:")
+        for official_name in found_list[1:]:
+            print("{}. {}".format(found_list.index(official_name), official_name))
+
+        image_index_to_pull = int(input("\nWhich image would you like to work on from the above list?\n"
+                                        "Enter the number which appears with the image name.\n"
+                                        "e.g. If you want to pull\n"
+                                        "3. tomcat7\n"
+                                        "Then type in 3 and press the Enter key.\n"))
+
+        image_name_to_pull = found_list[image_index_to_pull]
+        image_tags = list([''])
+        image_tags_request = requests.get(
+            "https://registry.hub.docker.com/v1/repositories/{}/tags".format(image_name_to_pull))
+        for image_tag_details in image_tags_request.json():
+            image_tags.append(image_tag_details['name'])
+
+        print("\n\nThe tags available for {} on Docker Hub are:\n".format(image_name_to_pull))
+        for tag_name in image_tags[1:]:
+            print("{}. {}".format(image_tags.index(tag_name), tag_name))
+
+        if image_repository_tags in image_tags[1:]:
+            print("Found specified {} tag in the available tags.".format(image_repository_tags))
+            image_tag_to_pull = image_repository_tags
+        else:
+            print("Could not find {} tag in the available tags.".format(image_repository_tags))
+            image_tag_to_pull = image_tags[
+                int(input("Which {} tag do you want to work with?\n".format(image_name_to_pull)))]
+
+        image_to_pull = "{}:{}".format(image_name_to_pull, image_tag_to_pull)
+        print("Pulling {} now...".format(image_to_pull))
+        pull_process(image_to_pull, client_instance)
+
+
+def search_images_local(opened_client, user_repotag):
     local_repotags = list()
-    for image_name in image_list:
+    for image_name in opened_client.images():
         for separate_tags in image_name['RepoTags']:
             local_repotags.append(separate_tags)
 
@@ -38,66 +95,40 @@ def pull_process(image_to_pull, opened_client):
     for pull_progress in opened_client.pull(image_to_pull, stream=True):
         for pull_element, pull_key in json.loads(pull_progress.decode()).items():
             if pull_element == 'id':
-                print("For image id {}:\n".format(pull_key.lower()), end=' ')
+                print("For image id {}:".format(pull_key.lower()))
             if pull_element == 'status':
-                print("The current status is {}\n\n".format(pull_key.lower()))
+                print("The current status is {}".format(pull_key.lower()))
 
 
-def start_container(image_repository, image_repository_tags):
+def start_container(opened_client, container_up):
+    pass
+    # container = ContainerClass()
+    # opened_client.create_container(image = container_up, command = container.container_command,
+    #                                hostname = container.container_hostname, mem_limit=container.container_mem_limit,
+    #                                dns=container.container_dns, name=container.container_name)
+
+
+def foreground_process(image_repository, image_repository_tags):
     # Establishing connection
     client_instance = Client(base_url=docker_config())
 
     user_repotag = "{}:{}".format(image_repository, image_repository_tags)
 
     # Searching for images locally
-    local_exists = search_images_local(client_instance.images(), user_repotag)
+    local_exists = search_images_local(client_instance, user_repotag)
 
     if local_exists:
-        print("Found the image {} locally, launching now.".format(user_repotag))
+        print("Found the image {} locally, proceeding now.".format(user_repotag))
     elif not local_exists:
         print("The image does not exist locally. "
               "HubOps will now search for images on Docker Hub.\n")
 
-        # User wants official or unofficial images?
-        while True:
-            official_or_not = input("Search for official images or unofficial images?\n"
-                                    "Official(O)\n"
-                                    "Unofficial(U)\n").lower()
-            if official_or_not in list("ou"):
-                break
+    get_image_remote(client_instance, image_repository, image_repository_tags)
 
-        found_list = search_images_name_remote(image_repository, client_instance, official_or_not)
-
-        if len(found_list) == 1:
-            print("No official image found for {}".format(image_repository))
-        else:
-            print("Official images on Docker Hub:")
-            for official_name in found_list[1:]:
-                print("{}. {}".format(found_list.index(official_name), official_name))
-
-            image_index_to_pull = int(input("\nWhich image would you like to work on from the above list?\n"
-                                            "Enter the number which appears with the image name.\n"
-                                            "e.g. If you want to pull\n"
-                                            "3. tomcat7\n"
-                                            "Then type in 3 and press the Enter key.\n"))
-
-            image_name_to_pull = found_list[image_index_to_pull]
-            image_tags = list([''])
-            image_tags_request = requests.get(
-                "https://registry.hub.docker.com/v1/repositories/{}/tags".format(image_name_to_pull))
-            for image_tag_details in image_tags_request.json():
-                image_tags.append(image_tag_details['name'])
-
-            print("\n\nThe tags available for {} on Docker Hub are:\n".format(image_name_to_pull))
-            for tag_name in image_tags[1:]:
-                print("{}. {}".format(image_tags.index(tag_name), tag_name))
-
-            image_tag_to_pull = image_tags[
-                int(input("Which {} tag do you want to work with?\n".format(image_name_to_pull)))]
-
-            image_to_pull = "{}:{}".format(image_name_to_pull, image_tag_to_pull)
-            print("Pulling {} now...".format(image_to_pull))
-            pull_process(image_to_pull, client_instance)
+    print('''
+    We will launch the container now.
+    ''')
+    start_container(client_instance, image_repository)
 
 
 if __name__ == '__main__':
@@ -113,4 +144,4 @@ if __name__ == '__main__':
         image_tag_argv = "latest"
         print("No image tag entered. Setting {} by default.".format(image_tag_argv))
 
-    start_container(image_name_argv, image_tag_argv)
+    foreground_process(image_name_argv, image_tag_argv)
