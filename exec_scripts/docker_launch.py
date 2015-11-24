55d0ea9c1f93c98afbc88c7ec45e163a82cbc546
Contains Cafeine <shubham@linux.com>
2015-11-24 16:19:44 +0530

    Introduced shell support for Vagrant since it has no API. Script now checks for initialized vagrant directory.

--- exec_scripts/docker_launch.py
+++ exec_scripts/docker_launch.py
@@ -1,4 +1,4 @@
-# /usr/bin/python3
+#!/usr/bin/python3
 
 from docker import Client
 import json, requests, sys
--- exec_scripts/vagrant_launch.py
+++ exec_scripts/vagrant_launch.py
@@ -1,3 +1,17 @@
-import subprocess
+#!/usr/bin/python3
 
-subprocess.call(['vagrant','init'])
+import subprocess, sys
+sys.path.append('/root/hubops')
+project_dir = '/root/hubops'
+
+def vagrant_health_checks():
+    #Check for vagrant init
+    current_status = subprocess.call(['{}/exec_scripts/vagrant_checks.sh'.format(project_dir),'c_init'])
+    if current_status == 0:
+        print("Vagrant was already initialized.")
+    elif current_status == 1:
+        print("Vagrant has been initialized.")
+
+
+if __name__ == '__main__':
+    vagrant_health_checks()
